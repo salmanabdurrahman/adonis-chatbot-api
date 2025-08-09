d29a60dad48ee90328872eeb35c1cb921800da26
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2025-08-10 03:00:47 +0700

    feat: integrate authentication and user management
    - Implemented authentication middleware to protect routes.
    - Added user model with UUID as primary key and password hashing.
    - Configured authentication guards for API access using OAT tokens.
    - Created migrations for users and API tokens tables.
    - Updated `.adonisrc.json` to include `@adonisjs/auth` provider.
    - Added silent authentication middleware for optional authentication checks.
    - Defined authentication contracts for providers and guards.

---
+++ app/Models/User.ts
@@ -0,0 +1,38 @@
+import { DateTime } from 'luxon'
+import Hash from '@ioc:Adonis/Core/Hash'
+import { column, beforeSave, BaseModel, beforeCreate } from '@ioc:Adonis/Lucid/Orm'
+import { v4 as uuidv4 } from 'uuid'
+
+export default class User extends BaseModel {
+  public static table = 'users'
+
+  @beforeCreate()
+  public static async setUUID(user: User) {
+    user.id = uuidv4()
+  }
+
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public rememberMeToken: string | null
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
+}
